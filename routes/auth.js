519ee1f3b5b1ce37d902319a9d13973c3ce612b9
AvinashMahanthi <avinashfab5678@gmail.com>
2021-05-09 00:19:14 +0530

    initial commitclear

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+
 
 mongoose.model('User')
 
@@ -23,15 +25,20 @@ router.post("/signup", (req, res) => {
             if (savedUser) {
         return res.status(422).json({error:"user already exists"})
             }
-            const user = new User({
-                email, password, name
-            })
-            user.save()
-                .then(user => {
-                res.json({ message: " saved sucessfully" })
-            }).catch(err => {
-                    console.log(err);
-                })
+            bcrypt.hash(password, 12)
+                .then(hashedpassword => {
+                    const user = new User({
+                        email,
+                        password:hashedpassword,
+                        name
+                    })
+                    user.save()
+                        .then(user => {
+                        res.json({ message: " saved sucessfully" })
+                    }).catch(err => {
+                            console.log(err);
+                        })
+            })     
         }).catch(err => {
             console.log(err);
         })
